a7c10153663f3edbe632bb553da1ccac596ae3cc
Jan Arnold <arnoldjan@yahoo.de>
2016-01-19 13:04:55 +0100

    init

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@
 'use strict';
 
 var appName = 'sample-angular-app';
+var angular = require('angular');
 
-angular.module(appName, []);
+require('mi-angular-resource-builder');
+require('angular-resource');
+var requires = [
+    'mi.ResourceBuilder',
+    'ngResource'
+];
+
+angular.module(appName, requires);
 angular.bootstrap(document, [appName]);
